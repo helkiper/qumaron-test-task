35eec6fd3a077437b3c2a14152267893858e5406
Artem Burbela <helkiper@gmail.com>
2021-08-27 14:45:01 +0300

    create entrypoint

---
+++ src/Main/Application.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Main;
+
+class Application
+{
+    private array $args;
+
+    public function __construct(array $args)
+    {
+        $this->args = $args;
+    }
+
+    public function run()
+    {
+        //todo
+    }
+}
